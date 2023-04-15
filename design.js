fe7c460fb77483b54ea9beac785fd938c83d3d43
Mouna bahria <mounabahria5@gmail.com>
2023-04-15 16:04:02 +0200

    first

---
+++ design.js
@@ -0,0 +1,27 @@
+
+$(".off-canvas-menu-btn").on('click', function () {
+    $("body").addClass('fix');
+    $(".off-canvas-menu-wrapper").addClass('open');
+});
+
+$(".btn-close-off-canvas,.off-canvas-overlay").on('click', function () {
+    $("body").removeClass('fix');
+    $(".off-canvas-menu-wrapper").removeClass('open');	});
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:true,
+    dots:false,
+    navText:['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
